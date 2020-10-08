fa00baae746f28fff51167a5b3eb02c72662944b
Serhii <ice2me1989.gmail.com>
2020-10-08 16:41:04 +0300

    first commit

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../Footer/Footer'
+import Header from '../Header/Header'
+
+import './App.css'
+
+function App() {
+	return (
+		<div className="App">
+			<Header />
+			<Footer />
+		</div>
+	);
+}
+
+export default App
