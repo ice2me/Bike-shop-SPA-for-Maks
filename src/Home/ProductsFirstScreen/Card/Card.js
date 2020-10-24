677b935373e243409139080fbeb3f2a059b03170
Serhii <ice2me1989.gmail.com>
2020-10-24 22:59:25 +0300

    first commit

--- src/Home/ProductsFirstScreen/Card/Card.js
+++ src/Home/ProductsFirstScreen/Card/Card.js
@@ -2,125 +2,103 @@ import React, { Component } from 'react'
 import './Card.css'
 import CardProps from './CardProps/CardProps'
 
-import img1 from '../../../Images/bikeOne.jpg'
-import img2 from '../../../Images/bikeTwo.jpg'
-import img3 from '../../../Images/bikeThree.jpg'
-import img4 from '../../../Images/helmet.jpg'
-import img5 from '../../../Images/link.jpg'
-import img6 from '../../../Images/speedometr.jpg'
-import img7 from '../../../Images/stars.jpg'
-import img8 from '../../../Images/tire.jpg'
-import img9 from '../../../Images/triger.jpg'
 
 export default class Card extends Component {
-	constructor(props){
-		super(props)
-		this.state = {
-			number: {
-				'001' : 'bike 001',
-				'002' : 'bike 002',
-				'003' : 'bike 003',
-				'004' : 'bike 004',
-				'005' : 'bike 005',
-				'006' : 'bike 006',
-				'007' : 'bike 007',
-				'008' : 'bike 008',
-				'009' : 'bike 009',
-				'010' : 'bike 010',
-			},
-			title: {
-				'001' : 'Lorem ipsum dolor sit amet.',
-				'002' : 'Lorem ipsum dolor sit amet consectetur adipisicing',
-				'003' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus.',
-				'004' : 'Lorem ipsum dolor sit amet.',
-				'005' : 'Lorem ipsum dolor sit amet consectetur adipisicing',
-				'006' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus.',
-				'007' : 'Lorem ipsum dolor sit amet.',
-				'008' : 'Lorem ipsum dolor sit amet consectetur adipisicing',
-				'009' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus.',
-				'010' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus.'
-			},
-			price: {
-				'001' : '1000',
-				'002' : '2000',
-				'003' : '3000',
-				'004' : '4000',
-				'005' : '5000',
-				'006' : '6000',
-				'007' : '7000',
-				'008' : '8000',
-				'009' : '9000',
-				'010' : '10000',
-			}
-		}
-	}
 	render() {
+		
 		return (
-			<div className='range__cards' onClick={this.clickOnCard}>
-				<CardProps
-					id='bikeOne'
-					number={this.state.number['001']}
-					src={img1}
-					title={this.state.title['001']}
-					price={this.state.price['001']}
-				/>
-				<CardProps
-					id='bikeTwo'
-					number={this.state.number['002']}
-					src={img2}
-					title={this.state.title['002']}
-					price={this.state.price['002']}
-				/>
-				<CardProps
-					id='bikeThree'
-					number={this.state.number['003']}
-					src={img3}
-					title={this.state.title['003']}
-					price={this.state.price['003']}
-				/>
-				<CardProps
-					number={this.state.number['004']}
-					src={img4}
-					title={this.state.title['004']}
-					price={this.state.price['004']}
-				/>
-				<CardProps
-					number={this.state.number['005']}
-					src={img5}
-					title={this.state.title['005']}
-					price={this.state.price['005']}
-				/>
-				<CardProps
-					number={this.state.number['006']}
-					src={img6}
-					title={this.state.title['006']}
-					price={this.state.price['006']}
-				/>
-				<CardProps
-					number={this.state.number['007']}
-					src={img7}
-					title={this.state.title['007']}
-					price={this.state.price['007']}
-				/>
-				<CardProps
-					number={this.state.number['008']}
-					src={img8}
-					title={this.state.title['008']}
-					price={this.state.price['008']}
-				/>
-				<CardProps
-					number={this.state.number['009']}
-					src={img9}
-					title={this.state.title['009']}
-					price={this.state.price['009']}
-				/>
-				<CardProps
-					number={this.state.number['010']}
-					src={img9}
-					title={this.state.title['010']}
-					price={this.state.price['010']}
-				/>
-			</div>
+			<>
+				{stateCard.map(item =>
+					<CardProps
+						number={item.number}
+						image={item.image}
+						title={item.title}
+						price={item.price}
+						key={item.key}
+					/>
+				)}
+			</>
 		)
 	}
 }
+
+
+const stateCard = [
+	{
+		"number": "bike 001",
+		'title' : 'Lorem ipsum dolor sit amet.',
+		'price' : '1000',
+		'image': '../../../Images/bikeOne.jpg',
+		'key': '00001'
+	},
+	{
+		"number": "bike 002",
+		'title' : 'Lorem ipsum dolor sit amet consectetur adipisicing',
+		'price' : '2000',
+		'image': '../../../Images/bikeTwo.jpg',
+		'key': '00002'
+	},
+	{
+		"number": "bike 003",
+		'title' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus.',
+		'price' : '3000',
+		'image': '../../../Images/bikeThree.jpg',
+		'key': '00003'
+	},
+	{
+		"number": "bike 004",
+		'title' : 'Lorem ipsum dolor sit amet.',
+		'price' : '4000',
+		'image': '../../../Images/helmet.jpg',
+		'key': '00004'
+	},
+	{
+		"number": "bike 005",
+		'title' : 'Lorem ipsum dolor sit amet consectetur adipisicing',
+		'price' : '5000',
+		'image': '../../../Images/link.jpg',
+		'key': '00005'
+	},
+	{
+		"number": "bike 006",
+		'title' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus.',
+		'price' : '6000',
+		'image': '../../../Images/speedometr.jpg',
+		'key': '00006'
+	},
+	{
+		"number": "bike 007",
+		'title' : 'Lorem ipsum dolor sit amet.',
+		'price' : '7000',
+		'image': '../../../Images/stars.jpg',
+		'key': '00007'
+	},
+	{
+		"number": "bike 008",
+		'title' : 'Lorem ipsum dolor sit amet consectetur adipisicing',
+		'price' : '8000',
+		'image': '../../../Images/tire.jpg',
+		'key': '00008'
+	},
+	{
+		"number": "bike 009",
+		'title' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus.',
+		'price' : '9000',
+		'image': '../../../Images/triger.jpg',
+		'key': '00009'
+	},
+	{
+		"number": "bike 010",
+		'title' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus.',
+		'price' : '10000',
+		'image': '../../../Images/triger.jpg',
+		'key': '00010'
+	},
+	{
+		"number": "bike 010",
+		'title' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus.',
+		'price' : '10000',
+		'image': '../../../Images/triger.jpg',
+		'key': '00011'
+	},
+]
--- src/Home/ProductsFirstScreen/Card/CardProps/CardProps.js
+++ src/Home/ProductsFirstScreen/Card/CardProps/CardProps.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import './CardProps.css'
 
-export default (props) => (
-	<div className='card'>
-		<div className="card__number">
-			<h2>{props.number}</h2>
+
+function CardProps(props){
+	return(
+		<div className='card'>
+			<div className="card__number">
+				<h2>{props.number}</h2>
+			</div>
+			<div className="card__img">
+				<img src={props.image} alt="card"/>
+			</div>
+			<div className="card__title">
+				<h4>{props.title}</h4>
+			</div>
+			<div className="card__price">
+				<span>{props.price}</span>
+				<p>грн</p>
+			</div>
+			<button className='card__button'>Купить</button>
 		</div>
-		<div className="card__img">
-			<img src={props.src} alt="card"/>
-		</div>
-		<div className="card__title">
-			<h4>{props.title}</h4>
-		</div>
-		<div className="card__price">
-			<span>{props.price}</span>
-			<p>грн</p>
-		</div>
-		<button className='card__button'>Купить</button>
-	</div>
-)
+	)
+}
+export default CardProps
